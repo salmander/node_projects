dcd2e5a67a3951a19f5f40a4c65bb0d3072df3b5
salmander <root@homestead>
2015-03-18 22:39:50 +0000

    added through2 module

--- stream-adventure/7.js
+++ stream-adventure/7.js
@@ -1,10 +1,22 @@
 // Http Server to return response UPPERCASED for only POST requests
 var http = require('http');
+var through = require('through2');
 
 var server = http.createServer(function (req, resp) {
-    if (req.method === 'POST') {
-        console.log(req.params);
+    console.log("Request Method: " + req.method + "\n");
+    if (req.method == 'POST') {
+        console.log(req.param);
+        req.on('data', function(data) {
+            //console.log('Data received.');
+            //console.log('data: ' + data);
+        });
+
+        req.on('end', function() {
+            resp.end();
+        });
+
+        req.pipe(through()).pipe(resp);
     }
 
-    resp.end('End');
+    //resp.end('End');
 }).listen(process.argv[2]);
